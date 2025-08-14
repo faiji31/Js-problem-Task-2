611d24cabe064da97dc470b0e432da1ba65c0ab3
Md.Faiji Akbar <faijiakbarliam2016@gmail.com>
2025-08-14 10:19:37 +0600

    5th commit

---
+++ problem4.js
@@ -0,0 +1,22 @@
+ const phones = [
+        { model: "PhoneA", brand: "Iphone", price: 95000 },
+        { model: "PhoneB", brand: "Samsung", price: 40000 },
+        { model: "PhoneC", brand: "Oppo", price: 26000 },
+        { model: "PhoneD", brand: "Nokia", price: 35000 },
+        { model: "PhoneE", brand: "Iphone", price: 105000 },
+        { model: "PhoneF", brand: "HTC", price: 48000 },
+    ];
+
+    function findAveragePhonePrice(phones){
+        let sum =0;
+        for (const i of phones){
+            sum=sum+i.price;
+            
+        }
+        const avg = sum/phones.length;
+        return avg;
+        
+    }
+
+    const liam = findAveragePhonePrice(phones);
+    console.log(liam);
